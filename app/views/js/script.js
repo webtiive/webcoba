de0fb08764b0a7f707cc837687d163e15109a65a
Joshuaaa1 <93573198+Joshuaaa1@users.noreply.github.com>
2022-10-18 17:30:27 +0800

    membuat popup whatsapp

--- app/views/js/script.js
+++ app/views/js/script.js
@@ -35,3 +35,16 @@ const container = document.querySelector(".navbar");
 hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
 });
+
+// Consult Toggle
+const consultBtn = document.getElementById('consultBtn')
+const consultPopup = document.getElementById('consultPopup')
+const closeBtn = document.getElementById('close')
+
+consultBtn.addEventListener('click', () => {
+    consultPopup.classList.toggle('pop')
+})
+
+closeBtn.addEventListener('click', () => {
+    consultPopup.classList.remove('pop')
+})
