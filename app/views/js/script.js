427d5665faff5f0d6ad14df88d6836e593d29d27
Joshuaaa1 <93573198+Joshuaaa1@users.noreply.github.com>
2022-10-25 10:21:16 +0800

    Merge branch 'main' of https://github.com/webtiive/webcoba

--- app/views/js/script.js
+++ app/views/js/script.js
@@ -44,3 +44,20 @@ window.addEventListener('scroll', () => {
     }
 });
 
+let imgreload = [];
+
+imgreload[0] = '../img/img1.png';
+imgreload[1] = '../img/img2.png';
+imgreload[2] = '../img/img3.png';
+imgreload[3] = '../img/img4.png';
+imgreload[4] = '../img/img5.png';
+
+window.onload = function() {
+
+    const random = Math.floor(Math.random()* imgreload.length);
+    console.log(random);
+
+    const test = document.querySelector('.img-change').style.backgroundImage = `url(${imgreload[random]})`;
+    console.log(test);
+
+}
