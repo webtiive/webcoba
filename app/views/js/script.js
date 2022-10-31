35a4ef7c3bef18f6f33b963b6b6a784f2f45e80b
Joshuaaa1 <93573198+Joshuaaa1@users.noreply.github.com>
2022-10-31 20:10:55 +0800

    menambah sub-menu

--- app/views/js/script.js
+++ app/views/js/script.js
@@ -1,18 +1,15 @@
-// Dropdown
-// let htmlcssArrow = document.querySelector('.htmlcss-arrow');
-// const 
-// htmlcssArrow.onclick = function () {
-//   navLinks.classList.toggle('show1');
-// }
-// let moreArrow = document.querySelector(".more-arrow");
-// moreArrow.onclick = function() {
-//     navLinks.classList.toggle("show2");
-// }
-
-// let jsArrow = document.querySelector(".js-arrow");
-// jsArrow.onclick = function() {
-//     navLinks.classList.toggle("show3");
-// };
+let htmlcssArrow = document.querySelector('.htmlcss-arrow');
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle('show1');
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+    navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+    navLinks.classList.toggle("show3");
+};
 
 // hamburger Menu
 const hamburger_menu = document.querySelector('.hamburger-menu');
@@ -29,7 +26,6 @@ const closeBtn = document.getElementById('close');
 
 consultBtn.addEventListener('click', () => {
   consultPopup.classList.toggle('pop');
-  console.log("hI");
 });
 
 closeBtn.addEventListener('click', () => {
