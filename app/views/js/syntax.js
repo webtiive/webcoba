22b6e78d21368c4757cfa4339be39d4a55a9b079
Joshuaaa1 <93573198+Joshuaaa1@users.noreply.github.com>
2022-11-15 22:54:59 +0800

    menampilkan gambar owner dan quotes

--- app/views/js/syntax.js
+++ app/views/js/syntax.js
@@ -2,10 +2,11 @@ const splash = document.querySelector('.splash');
 
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
-        document.getElementById("splash").style.display = 'none';
-    }, 5000);
+        document.getElementById("splash").style.opacity = 0;
+    }, 4000);
 
     setTimeout(()=>{
         document.getElementById("fade").style.opacity = 0;
     }, 1000);
 })
+
