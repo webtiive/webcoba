3dd9254db0904972482174de1f89495eebfbe9ca
Joshuaaa1 <93573198+Joshuaaa1@users.noreply.github.com>
2022-11-15 19:02:35 +0800

    memperbaiki error intro

--- app/views/js/syntax.js
+++ app/views/js/syntax.js
@@ -2,7 +2,7 @@ const splash = document.querySelector('.splash');
 
 document.addEventListener('DOMContentLoaded', (e)=>{
     setTimeout(()=>{
-        document.getElementById("splash").style.opacity = 0;
+        document.getElementById("splash").style.display = 'none';
     }, 5000);
 
     setTimeout(()=>{
